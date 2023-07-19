747228a6015e663b437f0775011e24c3f690452a
Kim Paulissen <kim.paulissen@kuleuven.be>
2023-07-19 16:44:12 +0200

    add upgrade-helper

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./css/");
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("js");
+  // eleventyConfig.addPassthroughCopy("./css/");
+  // eleventyConfig.addPassthroughCopy("images");
+  // eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addWatchTarget("./css/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addPlugin(UpgradeHelper);
 
   return {
     htmlTemplateEngine: "njk",
